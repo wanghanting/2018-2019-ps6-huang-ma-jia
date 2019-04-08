e0907cf0ba252cb8c538fadc2db6daca1c654afb
wanghanting <hanting.wang@etu.unice.fr>
2019-04-09 00:06:04 +0200

    #4 rajoute des method pour internship dans back-end

--- back/app/api/internship/index.js
+++ back/app/api/internship/index.js
@@ -4,6 +4,13 @@ const { Country } = require('../../models');
 
 const router = new Router();
 router.get('/', (req, res) => res.status(200).json(Internship.get()));
+
+function getByCountryIdFromEntreprise(company){
+  return company.internship.id;
+}
+
 router.get('/country=:countryId', (req, res) => res.status(200).json(Internship.getByCountryIdFromEntreprise(req.params.countryId)));
+router.get('/company=:companyId', (req, res) => res.status(200).json(Internship.getByInternshipId(req.params.companyId)));
+router.get('/internship=:internshipId', (req, res) => res.status(200).json(Internship.getByInternshipId(req.params.internshipId)));
 
 module.exports = router;
