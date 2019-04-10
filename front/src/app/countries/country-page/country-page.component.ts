d297f67db19fddf92580896cce1b31abdb9265a8
MaelDelaby <maeldelabouy@yahoo.fr>
2019-04-10 11:23:39 +0200

    ajout des filtre pour les entreprise dans country

--- front/src/app/countries/country-page/country-page.component.ts
+++ front/src/app/countries/country-page/country-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { OneCountryService } from '../../../services/country/one-country.service';
 import { PartnerHousingService } from '../../../services/partnerHousing/partnerHousing.service';
 import { CompanyService } from '../../../services/company/company.service';
@@ -8,19 +9,28 @@ import { PartnerHousing } from '../../../models/partnerHousing';
 import { Company } from '../../../models/company';
 
 @Component({
-  selector: 'app-country-list',
+  selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.scss']
 })
 export class CountryPageComponent implements OnInit {
 
     public country: Country
-    public partnerHousingList: PartnerHousing[];
-    public companyList: Company[];
+    public countryPageForm: FormGroup
+    public partnerHousingList: PartnerHousing[]
+    public companyList: Company[]
     public visaFullStarsArray: any[]
     public visaEmptyStarsArray: any[]
+    public filliereArray: any[]
+    public specialiteArray: any[]
+    public secteurArray: any[]
+    public sizeArray: any[]
 
-  constructor(public oneCountryService: OneCountryService, public partnerHousingService: PartnerHousingService,public companyService: CompanyService , private route: ActivatedRoute) {//Id par la suite
+  constructor(public formBuilder: FormBuilder, public oneCountryService: OneCountryService, public partnerHousingService: PartnerHousingService,public companyService: CompanyService , private route: ActivatedRoute) {//Id par la suite
+    this.filliereArray = ['salut', 'coucou', 'bonjour'];
+    this.specialiteArray = [];
+    this.secteurArray = [];
+    this.sizeArray = [];
     this.oneCountryService.country$.subscribe((country) => {
         this.country = country
         if (country != null){
@@ -39,8 +49,30 @@ export class CountryPageComponent implements OnInit {
         this.partnerHousingService.setCountryId(params['id']);
         this.companyService.setCountryId(params['id']);
     });
+
+    this.countryPageForm = this.formBuilder.group({
+      filliere: [''],
+      specialite: [''],
+      secteur: [''],
+      size: ['']
+    });
   }
 
   ngOnInit() {
+  }
+
+  filliereChange(value){
+
+    switch (value){
+      case 'salut':
+        this.secteurArray = ['salut', 'salut']
+        break;
+      case 'coucou':
+        this.secteurArray = ['c', 'c']
+        break;
+      case 'bonjour':
+        this.secteurArray = ['b', 'b']
+        break;
+    }
   }
 }
