0a08902899b0a80ffd5ca894cddfda0b09248c26
wanghanting <hanting.wang@etu.unice.fr>
2019-04-22 22:37:14 +0200

    #4 fix la port error

--- back/app/build-server.js
+++ back/app/build-server.js
@@ -13,5 +13,5 @@ module.exports = (cb) => {
   app.use(morgan('[:date[iso]] :method :url :status :response-time ms - :res[content-length]'));
   app.use('/api', api);
   app.use('*', (req, res) => res.status(404).end());
-  const server = app.listen(process.env.PORT || 4200, () => cb && cb(server));
+  const server = app.listen(process.env.PORT || 9428, () => cb && cb(server));
 };
