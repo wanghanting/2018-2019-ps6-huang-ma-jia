85ba65da3a30778f72b3802d00c8c836b4f15643
MaelDelaby <maeldelabouy@yahoo.fr>
2019-04-23 10:26:55 +0200

    specialities modif path

--- back/app/api/specialties/index.js
+++ back/app/api/specialties/index.js
@@ -3,18 +3,21 @@ const sector = require ('../../models/sector.model.js');
 
 
 const router = new Router();
-router.get('/', (req, res) => res.status(200).json(sector));
-router.get('/sector=:sector', (req, res) => {
-    var sectorFinded = false;
-    Object.values(sector).forEach((obj) => {
-        if (obj.name == req.params.sector){
-            res.status(200).json(Object.values(obj.specialties));
-            sectorFinded = true;
+router.get('/', (req, res) => {
+    if (req.query.sector) {
+        var sectorFinded = false;
+        Object.values(sector).forEach((obj) => {
+            if (obj.name == req.query.sector){
+                res.status(200).json(Object.values(obj.specialties));
+                sectorFinded = true;
+            }
+        })
+    
+        if (sectorFinded == false){
+            res.status(200).json([]);
         }
-    })
-
-    if (sectorFinded == false){
-        res.status(200).json([]);
+    } else {
+        res.status(200).json(sector)
     }
 });
 
