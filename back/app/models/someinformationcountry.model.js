9686538b673bb3aa33fff5ac5946b2dcf4e26203
Remy Larroye <remy.larroye@etu.unice.fr>
2019-04-30 00:00:54 +0200

    Modification de id de someInformationCountry

--- back/app/models/someinformationcountry.model.js
+++ back/app/models/someinformationcountry.model.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const BaseModel = require('../utils/base-model.js');
 
 module.exports = new BaseModel('SomeInformationCountry', {
-  id: Joi.number().required(),
+  id: Joi.string().required(),
   name: Joi.string().required(),
   pathFlag: Joi.string().required(),
   visaStar: Joi.number().required(),
