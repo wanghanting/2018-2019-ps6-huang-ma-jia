1cd6f8a4266763c742fd19931c17b9be73c3f0b7
MaelDelaby <maeldelabouy@yahoo.fr>
2019-05-07 09:57:44 +0200

    suppression someIbnformatiocountry

--- back/app/api/index.js
+++ back/app/api/index.js
@@ -7,7 +7,6 @@ const InternshipRouter = require('./internships');
 const SpecialtyRouter = require('./specialties');
 const SectorRouter = require('./sectors');
 const CompanySizeRouter = require('./companySizes');
-const SomeInformationCountry  = require('./someInformationCountry');
 const ActivitySectorsRouter  = require('./activitySectors');
 
 const router = new Router();
@@ -20,7 +19,6 @@ router.use('/internships', InternshipRouter);
 router.use('/specialties', SpecialtyRouter);
 router.use('/sectors', SectorRouter);
 router.use('/companySizes', CompanySizeRouter);
-router.use('/someInformationCountry', SomeInformationCountry);
 router.use('/activitySectors', ActivitySectorsRouter);
 
 module.exports = router;
--- back/app/models/index.js
+++ back/app/models/index.js
@@ -4,8 +4,7 @@ const Company = require('./company.model.js');
 const Country = require('./country.model.js');
 const PartnerHousing = require('./partnerHousing.model.js');
 const Internship = require('./internship.model.js');
-const SomeInformationCountry = require('./someinformationcountry.model');
 
 module.exports = {
-  Ticket, Student, Company, Country, PartnerHousing, Internship, SomeInformationCountry,
+  Ticket, Student, Company, Country, PartnerHousing, Internship,
 };
