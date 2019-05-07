730b6ab71c31af7f7ff3e6c4444ecddb746faecc
wanghanting <hanting.wang@etu.unice.fr>
2019-05-07 03:03:44 +0200

    #4 modifie des attribut dans des html dans front-end

--- front/src/app/company/search-form/search-form.component.ts
+++ front/src/app/company/search-form/search-form.component.ts
@@ -11,7 +11,7 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class SearchFormComponent implements OnInit {
 
   public searchForm: FormGroup;
-  private nameList = ['A ete reconduit CDD/CDI', 'Voiture de fonction'];
+  public nameList = ['A ete reconduit CDD/CDI', 'Voiture de fonction'];
 
 
   constructor(public formBuilder: FormBuilder, public  internshipservice: InternshipService) {
@@ -36,7 +36,7 @@ export class SearchFormComponent implements OnInit {
 
   internshipFilter() {
     const conditionsInternship: searchInternship = this.searchForm.getRawValue() as searchInternship;
-    const obj = {name: conditionsInternship.name, hasCDIouCDD: conditionsInternship.contractRenewed, hasVoiture: conditionsInternship.hasCompanyCar};
+    const obj = {hasCDIouCDD: conditionsInternship.contractRenewed, hasVoiture: conditionsInternship.hasCompanyCar};
     console.log(obj);
     // this.internshipservice.filterInternships(conditionsInternship.contractRenewed, conditionsInternship.hasCompanyCar);
     this.internshipservice.formChange(this.searchForm)
@@ -63,12 +63,6 @@ export class SearchFormComponent implements OnInit {
     // this.listFor(newList);
   }
 
-  onBlur() {
-    const selList = document.getElementById('sel');
-    // const list = document.getElementById('list');
-    selList.style.display = 'none';
-    // list.innerHTML = '';
-  }
 
   onFocus(){
     const selList = document.getElementById('sel');
@@ -76,11 +70,8 @@ export class SearchFormComponent implements OnInit {
   }
 
   onClick(){
-    const contract = document.getElementById('contract')
-    const voiture = document.getElementById('car')
     const input = (<HTMLSelectElement>document.getElementById('txt'))
     const selList = (<HTMLSelectElement>document.getElementById('sel'))
-    const search = (<HTMLInputElement>document.getElementById('search-input'))
     selList.style.display = "none"
     input.value = selList.options[selList.selectedIndex].text;
     const conditionsInternship: searchInternship = this.searchForm.getRawValue() as searchInternship;
