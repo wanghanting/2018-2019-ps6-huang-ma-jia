0bf14d9f1ddc02cc1b7d6a538c0eb62be4a587cc
wanghanting <hanting.wang@etu.unice.fr>
2019-05-14 01:45:35 +0200

    tranduit en francais

--- back/app/models/partnerHousing.model.js
+++ back/app/models/partnerHousing.model.js
@@ -1,9 +1,9 @@
-const Joi = require('joi');
-const BaseModel = require('../utils/base-model.js');
-
-module.exports = new BaseModel('PartnerHousing', {
-  id: Joi.number().required(),
-  countryId: Joi.string().required(),
-  name: Joi.string().required(),
-  webSite: Joi.string().required(),
-});
+// const Joi = require('joi');
+// const BaseModel = require('../utils/base-model.js');
+//
+// module.exports = new BaseModel('PartnerHousing', {
+//   id: Joi.number().required(),
+//   countryId: Joi.string().required(),
+//   name: Joi.string().required(),
+//   webSite: Joi.string().required(),
+// });
